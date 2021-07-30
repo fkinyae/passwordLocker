f051312ed906ec14b6e1b216a355eaafe08fd921
Francis Kinyae <fmkinyae@gmail.com>
2021-07-30 22:37:15 +0300

    add function defining the properties of a user

--- locker.py
+++ locker.py
@@ -4,4 +4,14 @@ class User:
     '''
     user_list = [] #list of users.add
     
-    
+    def __init__(self, username, password ):
+        '''
+        defining the properties of a user
+        '''
+        self.username = username
+        self.password = password
+        
+        #save a user
+    def save_user(self):
+        User.user_list.append(self)
+        
