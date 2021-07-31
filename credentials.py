abd684c5c4e4b2f1d598c1563d23dbfa208b750f
Francis Kinyae <fmkinyae@gmail.com>
2021-07-31 23:20:04 +0300

    add a method for copying passwords

--- credentials.py
+++ credentials.py
@@ -1,5 +1,6 @@
 import random
 import string
+import pyperclip
 from locker import User
  
 class Credential:
@@ -47,6 +48,19 @@ def generate_password(self,length):
     result_str = ' '.join(random.choice(letters) for i in range(length))
     print("Random string of  length", length, "is:", result_str)
 generate_password(8)
+
+@classmethod     
+def search_credential(cls, account):
+    '''
+    method that returns credentials for an account
+    '''   
+    for credential in cls.credentials_list:
+        if credential.account == account:
+         return Credential 
      
+@classmethod
+def copy_password(cls, account):
+    exist_credentials = Credential.search_credential(account)
+    pyperclip.copy(exist_credentials.password)     
     
         
