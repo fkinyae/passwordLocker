c603e4f3db85a97f94a7ac79659432699378ec1d
Francis Kinyae <fmkinyae@gmail.com>
2021-07-31 11:52:48 +0300

    add test for a saving user

--- locker_test.py
+++ locker_test.py
@@ -18,6 +18,13 @@ class TestUser(unittest.TestCase):
             self.assertEqual(self.new_user.username,'Francis')
             self.assertEqual(self.new_user.password,'khyegsbh')
             
+        def test_save_user(self):
+            '''
+            test_save_user test case to test if the user is saved on the user_list
+            '''
+            self.new_user.save_user() #save the user
+            self.assertEqual(len(User.user_list),1)
+            
     if __name__ == '_main_':
         unittest.main()        
 
