3d90fbf7e47819ed3d49a85ad1e406e96d4db6a1
Francis Kinyae <fmkinyae@gmail.com>
2021-07-31 22:18:30 +0300

    add a method for saving credentials

---
+++ credentials.py
@@ -0,0 +1,33 @@
+ from locker import User
+ 
+class Credential:
+            '''
+            the blueprint for creating new objects for credentials
+            '''
+            credentials_list = []
+            
+@classmethod
+def confirm_users(cls, username, password):
+        '''
+        method to confirm the user exists in the list
+        '''
+        current_user = ''
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                current_user = user.username
+                return current_user
+            
+def __init__(self, account, username, password):
+        '''
+        method that defines the types of user credentials to be saved
+        '''   
+        self.account = account
+        self.username = username
+        self.password = password
+        
+def save_credentials(self):
+        '''
+        method for storing new credentials
+        '''   
+        Credential.credentials_list.append(self)
+        
