8eb0e925a46504ee5385de60f52e2a7075bff2d7
Francis Kinyae <fmkinyae@gmail.com>
2021-08-04 01:16:38 +0300

    add setup method

---
+++ test_credential.py
@@ -0,0 +1,11 @@
+import unittest 
+from credential import Credential
+
+class TestCredential(unittest.TestCase):
+    '''
+    Test class that defines the test cases for the behaviour of the credential class
+    '''
+    def setUp(self):
+        '''
+        method to run before each test case 
+        '''
