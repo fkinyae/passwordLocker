c493cf4c89cd68ede73619ae7727ab91bf13d656
Francis Kinyae <fmkinyae@gmail.com>
2021-08-04 00:52:17 +0300

    create user class

---
+++ user.py
@@ -0,0 +1,27 @@
+class User:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    users_list = [] # Empty contact list
+
+    def __init__(self, username, password ):
+
+      # docstring removed for simplicity
+
+        self.username = username
+        self.password = password
+        
+    def save_user(self):
+        '''
+        method that saves a new instance of a user into the list
+        '''
+        User.users_list.append(self)
+        
+    def delete_user(self):   
+        '''
+        method for deleting a saved user from the users list
+        '''
+        User.users_list.remove(self)
+        
+        
