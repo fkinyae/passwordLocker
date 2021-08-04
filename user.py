6a5de848b5cf324398d37b20c9f9e6a045d1be79
Francis Kinyae <fmkinyae@gmail.com>
2021-08-04 12:55:06 +0300

    add a password generator function

--- user.py
+++ user.py
@@ -1,3 +1,5 @@
+import random
+import string
 class User:
     """
     Class that generates new instances of contacts.
@@ -23,5 +25,25 @@ class User:
         method for deleting a saved user from the users list
         '''
         User.users_list.remove(self)
+        
+        
+    def gen_password():
+        print('Hello, generate your password here!')
+        
+        length = int(input('\nEnter the password length:'))
+        
+        lower = string.ascii_lowercase
+        upper = string.ascii_uppercase
+        num = string.digits
+        symbols = string.punctuation
+        
+        all = lower + upper + num + symbols
+        
+        temp = random.sample(all, length)
+        
+        password = "".join(temp)
+        
+        print(password)
+
         
         
