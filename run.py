0573f891eec4dba1508e4d1b2d9f0837d35e814b
Francis Kinyae <fmkinyae@gmail.com>
2021-08-04 13:32:48 +0300

    create a save user function

---
+++ run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.9
+from credential import Credential
+from user import User
+
+def create_new_user(username, password):
+    '''
+    Function that creates a user
+    '''
+    new_user = User(username, password)
+    return new_user
+
+def save_user(user):
+    '''
+    function to save user
+    '''
+    user.save_user()
+    
+def del_user(user):
+    '''
+    Function that deletes a user
+    '''  
+    user.delete_user()
+    
+def user_login():
+    '''
+    check user authentication
+    '''  
+      
