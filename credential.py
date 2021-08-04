62f5eede4a489c7207ad442fcfd2f02009daa68b
Francis Kinyae <fmkinyae@gmail.com>
2021-08-04 10:35:17 +0300

    add a classmethod to pass the value of cls

--- credential.py
+++ credential.py
@@ -1,58 +1,58 @@
 from user import User
 
+
 class Credential:
     '''
     the blueprint for creating new objects for credentials
     '''
     credentials_list = []
-            
+
+    @classmethod
     def authenticate_user(cls, username, password):
         '''
+        classmethod
         check whether the user is in our list
-        '''  
-        firstuser = ""   
+        '''
+        firstuser = ""
         for user in User.users_list:
-            if (user.username == username and user.password == password ):
+            if (user.username == username and user.password == password):
                 firstuser == user.username
         return firstuser
-    
+
     def __init__(self, account, userName, passWord):
         '''
         method that initializes the credentials to be stored
-        '''   
+        '''
         self.account = account
-        self.userName = userName    
-        self.passWord = passWord 
-        
+        self.userName = userName
+        self.passWord = passWord
+
     def save_credential(self):
         '''
         method to save credentials
-        '''    
+        '''
         Credential.credentials_list.append(self)
-        
-    def delete_credential(self):  
+
+    def delete_credential(self):
         '''
         method for deleting a credential from the list
-        ''' 
+        '''
         Credential.credentials_list.remove(self)
-        
-    @classmethod  
+
+    @classmethod
     def find_by_account(cls, account):
         '''
         method to search credentials by account
-        ''' 
+        '''
         for credential in cls.credentials_list:
             if credential.account == account:
                 return credential
             
-    def   credential_exist(cls, account):
+    @classmethod
+    def find_credential(cls, account):
         '''
         method to check if credential exists
-        '''    
+        '''
         for credential in cls.credentials_list:
             if credential.account == account:
-                return True
-        
-        return False    
-        
-            
+                return credential
